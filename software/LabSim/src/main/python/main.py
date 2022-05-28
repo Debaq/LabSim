1b4b75397c177292a922df750f8a206423e59a53
Nicolás Quezada <david.avila@uach.cl>
2022-05-28 12:54:25 -0400

    Update main.py

--- software/LabSim/src/main/python/main.py
+++ software/LabSim/src/main/python/main.py
@@ -6,21 +6,25 @@ class IntencityModel:
     """IntencityModel class
     Maneja la intensidad del estimulo según sea necesario
     """
-    def __init__(self, limits:dict):
-        self.list_frecuencies = [125, 250, 500, 1000, 2000, 3000, 4000, 6000, 8000]
-        self.limits = limits
+    def __init__(self,_limits:dict, frequency:list=None) -> None:
+        if frequency is None:
+            self.list_frecuencies = [125, 250, 500, 1000, 2000, 3000, 4000, 6000, 8000]
+        else:
+            self.list_frecuencies = frequency
+        self.limits = _limits
         self.frecuency = 1000
         self.intencity = 20
         self.exted_range = 20
         self.steps = 5
-        self.range_intencity = self.create_range_intencity()
         self.load_limits(self.frecuency)
+        self.range_intencity = self.create_range_intencity()
 
 
     def load_limits(self, frecuency:int) -> int:
-        limits = self.limits(frecuency)
-        self.max_intencity = limits[0]
-        self.min_intencity = limits[1]
+
+        _limits = self.limits[frecuency]
+        self.max_intencity = _limits[0]
+        self.min_intencity = _limits[1]
         
     def next(self):
         idx_f = self.list_frecuencies.index(self.frecuency)
@@ -62,11 +66,11 @@ class IntencityModel:
 
 
 
-limits = { 125: (-15, 120), 250 : (-15, 120), 500 : (-15, 120), 
-          1000 : (-15, 120), 2000 : (-15, 120), 3000 : (-15, 120), 
+limits = { 125: (-15, 120), 250 : (-15, 120), 500 : (-15, 120),
+          1000 : (-15, 120), 2000 : (-15, 120), 3000 : (-15, 120),
           4000 : (-15, 120), 6000 : (-15, 120), 8000 : (-15, 120)}
 
-model_inten = IntencityModel()
+model_inten = IntencityModel(limits)
 
 model_inten.set_frecuency(500)
 
